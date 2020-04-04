ca7e36a7315621108a7862ad939d0c42b6323646
Carlos Gálvez <carlosgalvezp@gmail.com>
2020-04-04 18:31:05 +0200

    Build with buildkit

--- build_docker.sh
+++ build_docker.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-cp requirements.txt docker
-docker build docker
+export DOCKER_BUILDKIT=1
+
+cp requirements.txt docker/
+docker build -t carlosgalvezp/cvnd_p2_image_captioning:latest docker/
 rm docker/requirements.txt
