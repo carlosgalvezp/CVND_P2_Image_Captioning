bba06ebce7138d30bbdae518613eb29a2b7d2937
Carlos Gálvez <carlosgalvezp@gmail.com>
2020-04-05 14:02:47 +0200

    Implement RNNDecoder

--- model.py
+++ model.py
@@ -23,11 +23,46 @@ class EncoderCNN(nn.Module):
 
 class DecoderRNN(nn.Module):
     def __init__(self, embed_size, hidden_size, vocab_size, num_layers=1):
-        pass
-    
-    def forward(self, features, captions):
-        pass
+        super(DecoderRNN, self).__init__()
+        
+        self.embed_size = embed_size
+        
+        # Embedding layer that turns words into a vector of a specified size
+        self.word_embeddings = nn.Embedding(num_embeddings=vocab_size, embedding_dim=embed_size)
 
+        # LSTM takes embedded word vectors (of a specified size) as inputs 
+        # and outputs hidden states of size hidden_dim        
+        # Pass "batch_first=True" since our inputs and outputs have the batch as first dim
+        self.lstm = nn.LSTM(input_size=embed_size,
+                            hidden_size=hidden_size,
+                            num_layers=num_layers,
+                            batch_first=True)  
+        
+        # Linear layer that maps the hidden state output dimension 
+        # to the vocabulary size we want as output
+        self.hidden2tag = nn.Linear(hidden_size, vocab_size)        
+       
+    def forward(self, features, captions):
+        # Remove the last token from "captions" (<end>), see picture in Step 4
+        captions = captions[:, :-1]
+        
+        # Create embeddings from captions
+        captions_embeddings = self.word_embeddings(captions)
+        
+        # Concatenate features and embeddings. Need to add an extra dimension
+        # to the input features, since:
+        # features.shape            = [batch_size,                  embed_size]
+        # captions_embeddings.shape = [batch_size, sequence_length, embed_size]
+        features = features.view((-1, 1, self.embed_size))
+        lstm_input = torch.cat((features, captions_embeddings), dim=1)
+        
+        # Pass to LSTM
+        out, hidden = self.lstm(lstm_input)
+        
+        # Convert from hidden space to vocabulary space        
+        return self.hidden2tag(out)
+        
+        
     def sample(self, inputs, states=None, max_len=20):
         " accepts pre-processed image tensor (inputs) and returns predicted sentence (list of tensor ids of length max_len) "
         pass
